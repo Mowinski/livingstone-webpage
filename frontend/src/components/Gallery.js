6adefc46e51ad266b16690dbec21d951f4ccecd6
Kamil Mówiński <kamil.mowinski@mirumee.com>
2019-06-13 10:59:43 +0200

    Fix gallery template

--- frontend/src/components/Gallery.js
+++ frontend/src/components/Gallery.js
@@ -4,44 +4,48 @@ import {StateContext} from "./App";
 
 const Gallery = () => {
     const stateContext = React.useContext(StateContext);
-    const renderImage = (image) => {
-        let description = <></>;
-        if(image.description) {
-            description = (
-                <div className="product-item-description d-flex mr-auto">
-                    <div className="bg-faded p-5 rounded">
-                        <p className="mb-0">{image.description}</p>
-                    </div>
-                </div>);
-        }
+
+    const renderDescription = (description, floatingClass) => {
+        if(!description) return (<></>);
         return (
-            <div className="col-md-12 col-sm-12" key={image.id}>
-                <div className="product-item">
-                    <div className="product-item-title d-flex">
-                        <div className="bg-faded p-5 d-flex ml-auto rounded">
-                            <h2 className="section-heading mb-0">
-                                <span className="section-heading-upper">{image.name}</span>
-                            </h2>
-                        </div>
-                    </div>
-                    <img className="product-item-img mx-auto mx-sm-0 d-flex rounded mb-3 mb-lg-0 img-responsive" src={image.image}
-                         alt={image.name}/>
-                    {description}
+            <div className={"product-item-description d-flex " + floatingClass}>
+                <div className="bg-faded p-5 rounded">
+                    <p className="mb-0">{description}</p>
                 </div>
             </div>
+        )
+    };
+
+    const renderImage = (image, index) => {
+        const headerClass = index % 2 === 0 ? "ml-auto" : 'mr-auto';
+        const descriptionClass =  index % 2 === 0 ? "mr-auto" : 'ml-auto';
+        return (
+            <section className="page-section" key={image.id}>
+                <div className="container">
+                    <div className="product-item">
+                        <div className="product-item-title d-flex">
+                            <div className={"bg-faded p-5 d-flex rounded " + headerClass}>
+                                <h2 className="section-heading mb-0">
+                                    <span className="section-heading-upper">{image.name}</span>
+                                </h2>
+                            </div>
+                        </div>
+                        <img className="product-item-img mx-auto mx-xs-0 d-flex rounded mb-3 mb-lg-0 fluid"
+                             src={image.image}
+                             alt={image.name}/>
+                        {renderDescription(image.description, descriptionClass)}
+                    </div>
+                </div>
+            </section>
         );
     };
 
-    let images = stateContext.state.gallery_images.map((image) => renderImage(image));
+    let images = stateContext.state.gallery_images.map((image, index) => renderImage(image, index));
 
     return (
-        <section className="page-section">
-            <div className="container">
-                <div className="row">
-                    {images}
-                </div>
-            </div>
-        </section>
+        <>
+            {images}
+        </>
     );
 };
 
